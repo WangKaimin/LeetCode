4b0c65cbde53a62bedc38f8e95ecae0f33f83685
WangKaimin <info@wangkaimin.com>
2014-05-31 19:03:09 +0800

    add Single Number II

--- SingleNumberII.hpp
+++ SingleNumberII.hpp
@@ -6,20 +6,30 @@ using namespace std;
 class Solution {
 public:
     int singleNumber(int A[], int n) {
-        int t=0;
-        while(--n>=0)
-            t ^= A[n];
-        return t;
+        int m = sizeof(int)*8;
+        int w[m], result;
+        fill_n(&w[0], m, 0);
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+            {
+                w[j] += (A[i] >> j)&1;
+                w[j] %= 3;
+            }
+        result = 0;
+        for(int i=0; i<m; i++)
+            result += w[i]<<i;
+        return result;
     }
     void test()
     {
         Solution sol;
         int A[] = {1,4,6,1,4,6,3,4,1,6};
         int result;
-        for(int i=0; i<10; i++)
-            cout<<A[i]<<endl;
+        for(int i=0; i<10-1; i++)
+            cout<< A[i] << " ";
+        cout<<endl;
         result=sol.singleNumber(A, 10);
-        cout<< "Result = " <<result <<endl;
+        cout<< "Result = " << result <<endl;
 
     }
 };
