df23115648ef2ea1d2edeabc56703d1a95f3229b
WangKaimin <info@wangkaimin.com>
2014-06-04 23:26:01 +0800

    add

--- InsertInterval.hpp
+++ InsertInterval.hpp
@@ -17,9 +17,20 @@ public:
         vector<Interval>::iterator it = intervals.begin();
         while(it != internal.end())
         {
-            if(newInterval.end < it->start)
-            ;
+            if(newInterval.end < it->start) {
+                intervals.insert(it, newInterval);
+                return intervals;
+            }
+            else if(newInterval.start > it->end)
+                it++;
+            else {
+                newInterval.start = min(newInterval.start, it->start);
+                newInterval.end = max(newInterval.end, it->end);
+                it = intervals.erase(it);
+            }
         }
+        intervals.insert(intervals.end(), newInterval);
+        return intervals;
     }
 };
 
